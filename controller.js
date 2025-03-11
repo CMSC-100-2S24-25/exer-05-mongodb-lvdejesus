dd13268ee1f99261218fe8995c0309c719285d1e
lvdejesus <lvdejesus1@up.edu.ph>
2025-03-11 15:16:37 +0800

    feat: add CRUD operations

---
+++ controller.js
@@ -0,0 +1,62 @@
+import mongoose from "mongoose";
+
+const Student = mongoose.model("Student", {
+  stdnum: String,
+  fname: String,
+  lname: String,
+  age: Number,
+}, 'studentData');
+
+export const saveStudent = (req, res) => {
+  const { stdnum, fname, lname, age } = req.body;
+  if (stdnum == null || fname == null || lname == null || age == null) {
+    res.send({ inserted: false });
+    return;
+  }
+
+  // const foundStudent = Student.findOne({ stdnum });
+  // console.log(foundStudent);
+
+  const student = new Student({ stdnum, fname, lname, age });
+  student.save().then(() => {
+    res.send({ inserted: true });
+  }).catch(() => {
+    res.send({ inserted: false });
+  })
+}
+
+export const update = (req, res) => {
+  const { fname, newFname } = req.body;
+  Student.updateOne({ fname }, { $set: { fname: newFname } }).then(() => {
+    res.send({ updated: true });
+  }).catch(() => {
+    res.send({ updated: false });
+  })
+}
+
+export const removeUser = (req, res) => {
+  const { stdnum } = req.body;
+  Student.deleteOne({ stdnum }).then(() => {
+    res.send({ updated: true });
+  }).catch(() => {
+    res.send({ updated: false });
+  })
+}
+
+export const removeAllUser = (_req, res) => {
+  Student.deleteMany().then((_result) => {
+    res.send({ deleted: true });
+  }).catch(() => {
+    res.send({ deleted: false });
+  })
+}
+
+export const user = async (req, res) => {
+  const { stdnum } = req.query;
+  const result = await Student.find({ stdnum });
+  res.send(result);
+}
+
+export const members = async (_req, res) => {
+  res.send(await Student.find());
+}
--- index.js
+++ index.js
@@ -1,5 +1,12 @@
 import express from "express";
+import mongoose from "mongoose";
 import { router } from "./router.js";
+
+await mongoose.connect("mongodb://127.0.0.1:27017/StudentDatabase", {
+  useNewUrlParser: true, useUnifiedTopology: true
+}).catch(() => {
+  console.error("Connecting to database failed!");
+});
 
 const app = express();
 app.use(express.json());

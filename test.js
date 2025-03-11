9825394dff3ca3bdbc1c9685af5c3a0934c456da
lvdejesus <lvdejesus1@up.edu.ph>
2025-03-11 15:28:29 +0800

    test: add names to tests

--- test.js
+++ test.js
@@ -33,46 +33,47 @@ const students = [
   },
 ]
 
-const printResult = (err, result) => {
-  if (err != null)
-    console.error(err);
+const printResult = (name) => {
+  return (err, result) => {
+    if (err != null)
+      console.error(`${name}: `, err);
 
-  console.log(result.body);
-};
-
+    console.log(`${name}: `, result.body);
+  };
+}
 
 // Test /save-student
 for (let i = 0; i < students.length; i++) {
-  needle.post('http://localhost:3000/save-student', students[i], printResult);
+  needle.post('http://localhost:3000/save-student', students[i], printResult(`save-${i}`));
 }
 
 // Test /save-student if stdnum is the same
-needle.post('http://localhost:3000/save-student', students[0], printResult);
+needle.post('http://localhost:3000/save-student', students[0], printResult(`save-same`));
 
 // Test /save-student if the input is not complete
 needle.post('http://localhost:3000/save-student', {
   stdnum: "202510039",
   fname: "Erisa",
   age: 18,
-}, printResult);
+}, printResult(`save-incomplete`));
 
 // Test /update
-needle.post('http://localhost:3000/update', { fname: "Suu", newFname: "Su" }, printResult);
+needle.post('http://localhost:3000/update', { fname: "Suu", newFname: "Su" }, printResult(`update`));
 
 // Test /update if the student does not exist
-needle.post('http://localhost:3000/update', { fname: "Riri", newFname: "Su" }, printResult);
+needle.post('http://localhost:3000/update', { fname: "Riri", newFname: "Su" }, printResult(`update-not-exist`));
 
 // Test /remove-user
-needle.post('http://localhost:3000/remove-user', { stdnum: "202671239" }, printResult);
+needle.post('http://localhost:3000/remove-user', { stdnum: "202671239" }, printResult(`remove`));
 
 // Test /remove-user if the student does not exist
-needle.post('http://localhost:3000/remove-user', { stdnum: "202309283" }, printResult);
+needle.post('http://localhost:3000/remove-user', { stdnum: "202309283" }, printResult(`remove-not-exist`));
 
 // Test /user
-needle.get(`http://localhost:3000/user?stdnum=202511235`, printResult);
+needle.get(`http://localhost:3000/user?stdnum=202511235`, printResult(`user`));
 
 // Test /members
-needle.get('http://localhost:3000/members', printResult);
+needle.get('http://localhost:3000/members', printResult(`members`));
 
 // Test /remove-all-user
-needle.post('http://localhost:3000/remove-all-user', {}, printResult);
+needle.post('http://localhost:3000/remove-all-user', {}, printResult(`remove-all-user`));
